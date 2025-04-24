222c453b1d506ce0c5c20e47991f71c7b930faba
lorcai <ilorcaalonso@gmail.com>
2025-04-24 12:39:29 +0200

    Add gene expression violin plots

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -2,12 +2,14 @@ import { model } from '@platforma-open/milaboratories.cell-browser.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
 import UMAP from './pages/UMAP.vue';
+import GraphPage from './pages/GraphPage.vue';
 
 export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
       '/': () => MainPage,
       '/umap': () => UMAP,
+      '/graph': () => GraphPage,
     },
   };
 });
---
+++ ui/src/pages/GraphPage.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { GraphMakerProps } from '@milaboratories/graph-maker';
+import { GraphMaker } from '@milaboratories/graph-maker';
+import '@milaboratories/graph-maker/styles';
+import { ref } from 'vue';
+import { useApp } from '../app';
+import type { PColumnIdAndSpec } from '@platforma-sdk/model';
+
+// import '@milaboratories/graph-maker/styles';
+
+const app = useApp();
+
+function getDefaultOptions(violinExprPfDefaults?: PColumnIdAndSpec[]) {
+  if (!violinExprPfDefaults) {
+    return undefined;
+  }
+
+  function getIndex(name: string, pcols: PColumnIdAndSpec[]): number {
+    return pcols.findIndex((p) => p.spec.name === name);
+  }
+
+  const defaults: GraphMakerProps['defaultOptions'] = [
+    {
+      inputName: 'y',
+      selectedSource: violinExprPfDefaults[getIndex('pl7.app/rna-seq/countMatrix',
+        violinExprPfDefaults)].spec,
+    },
+    {
+      inputName: 'primaryGrouping',
+      selectedSource: violinExprPfDefaults[getIndex('pl7.app/label',
+        violinExprPfDefaults)].spec,
+    },
+    {
+      inputName: 'filters',
+      selectedSource: violinExprPfDefaults[getIndex('pl7.app/rna-seq/geneSymbols',
+        violinExprPfDefaults)].spec,
+    },
+  ];
+
+  return defaults;
+}
+
+const defaultOptions = ref(getDefaultOptions(app.model.outputs.violinExprPfDefaults));
+
+</script>
+
+<template>
+  <GraphMaker
+    v-model="app.model.ui.graphState" chartType="discrete"
+    :p-frame="app.model.outputs.violinExprPf" :default-options="defaultOptions"
+  />
+</template>
