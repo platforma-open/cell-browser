4c723c2585195eda4384da598a8608e4b0b60209
lorcai <ilorcaalonso@gmail.com>
2025-05-15 15:43:15 +0200

    Set heatmap defaults

--- ui/src/pages/heatmapPage.vue
+++ ui/src/pages/heatmapPage.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { GraphMaker, GraphMakerProps, PredefinedGraphOption } from "@milaboratories/graph-maker";
 import '@milaboratories/graph-maker/styles';
-import { useApp } from '../app';
 import { computed } from 'vue';
+import { useApp } from '../app';
 
 const app = useApp();
 
 const defaultOptions = computed((): GraphMakerProps['defaultOptions'] => {
-  if (!app.model.outputs.countsSpec) // will change the name, its actually the countsRef
+  if (!app.model.outputs.countsSpec)
     return undefined;
 
   const defaults: PredefinedGraphOption<'heatmap'>[] = [
@@ -22,24 +22,39 @@ const defaultOptions = computed((): GraphMakerProps['defaultOptions'] => {
       selectedSource: app.model.outputs.countsSpec.axesSpec[1],
     },
     {
+      // Sample ID also needed in X axis. Because cluster ID includes sample ID as axis
+      inputName: 'x',
+      selectedSource: app.model.outputs.countsSpec.axesSpec[0],
+    },
+    {
       // Gene ID as Y axis
       inputName: 'y',
       selectedSource: app.model.outputs.countsSpec.axesSpec[2],
     },
     {
-      // Sample as x group axis
+      // Cluster ID as x-group-by axis
       inputName: 'xGroupBy',
-      selectedSource: app.model.outputs.countsSpec.axesSpec[0],
+      selectedSource: {
+        kind: 'PColumn',
+        name: 'pl7.app/rna-seq/leidencluster',
+        valueType: 'String',
+        axesSpec: [],
+      },
     },
   ];
-  // Add default filter only if there is at least a DEG Pcolumn
-  if (app.model.outputs.DEGpf // Again its actually a log2fold change pcol subset for testing, seems to not work. Because of axes?
+  // Add default filter only if there is at least a DEG subset Pcolumn
+  if (app.model.outputs.DEGpf
     && app.model.outputs.DEGpf.length !== 0) {
     // Extend default options
     defaults.push({
       // DEG gene list (if present) as filter
       inputName: 'filters',
       selectedSource: app.model.outputs.DEGpf[0].spec,
+      // Set default filter to a specific cluster (else, will be set automatically)
+      /* fixedAxes: [{
+        axisIdx: 0,
+        axisValue: '0',
+      }], */
     });
   }
 
