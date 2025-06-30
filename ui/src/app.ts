0fa9c6e6c272b6065b1dca0983b45b4a49872ee2
aiturbidemil <aiturbide@milaboratories.com>
2025-06-30 11:40:25 +0200

    improved UI plus umap selection fix

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,7 +1,6 @@
 import { model } from '@platforma-open/milaboratories.cell-browser.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
-import UMAP from './pages/UMAP.vue';
 import ViolinPage from './pages/ViolinPage.vue';
 import heatmapPage from './pages/heatmapPage.vue';
 
@@ -9,7 +8,6 @@ export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
       '/': () => MainPage,
-      '/umap': () => UMAP,
       '/violin': () => ViolinPage,
       '/heatmap': () => heatmapPage,
     },
