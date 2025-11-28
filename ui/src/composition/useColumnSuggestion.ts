498cc592a50c4936d7b7db59e920e0e9c0956394
AStaroverov <hellbeast92@gmail.com>
2025-11-28 15:40:25 +0100

    chore: update package versions in pnpm-workspace.yaml
    feat: enhance useColumnSuggestion to support searchType parameter for value and label filtering

--- ui/src/composition/useColumnSuggestion.ts
+++ ui/src/composition/useColumnSuggestion.ts
@@ -9,14 +9,16 @@ import { useApp } from '../app';
 export function useColumnSuggestion() {
   const app = useApp();
 
-  const suggest = async (params: { columnId: string; searchStr: string; axisIdx?: number }): Promise<ListOptionBase<string | number>[]> => {
+  const suggest = async (params: { columnId: string; axisIdx?: number; searchStr: string; searchType: 'value' | 'label' }): Promise<ListOptionBase<string | number>[]> => {
     const handle = app.model.outputs.overlapColumns?.pFrame;
     if (handle == null) return [];
+
     const response = await getUniqueSourceValuesWithLabels(handle, {
       columnId: params.columnId as SUniversalPColumnId,
       axisIdx: params.axisIdx,
       limit: 300,
-      searchQuery: params.searchStr,
+      searchQuery: params.searchType === 'label' ? params.searchStr : undefined,
+      searchQueryValue: params.searchType === 'value' ? params.searchStr : undefined,
     }).catch((err) => {
       console.error('Error while fetching unique values for suggestion:', err);
       return ({ values: [] as ListOptionBase<string | number>[] });
